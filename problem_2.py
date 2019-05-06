5e43e4d5790ca42bb580ba3904dd386845b44a44
Wojciech Tomaszewski <wojciech.tomaszewski1@ibm.com>
2019-05-06 11:18:30 +0200

    p2 prefix/sufix method

--- problem_2.py
+++ problem_2.py
@@ -8,11 +8,40 @@ Follow-up: what if you can't use division?
 
 from functools import reduce
 
-
+# O(n)
 def gen_array(a):
     return [reduce(lambda x,y: x * y, a[:i] + a[i+1:]) for i in range(len(a))]
 
+# O(n)
+def gen_array2(a):
+    # Generate prefix/suffix lists
+    prefix = []
+    for num in a:
+        if prefix:
+            prefix.append(prefix[-1] * num)
+        else:
+            prefix.append(num)
+
+    suffix = []
+    for num in reversed(a):
+        if suffix:
+            suffix.append(suffix[-1] * num)
+        else:
+            suffix.append(num)
+    suffix = list(reversed(suffix))
+
+    result = []
+
+    for i in range(len(a)):
+        if i == 0:
+            result.append(suffix[i + 1])
+        elif i == len(a) - 1:
+            result.append(prefix[i -1])
+        else:
+            result.append(prefix[i - 1] * suffix[i + 1])
+    return result
 
 if __name__ == "__main__":
     a = [1, 2, 3, 4, 5]
     print(gen_array(a))
+    print(gen_array2(a))
